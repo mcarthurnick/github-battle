52623e1fee69220c92663fd4088f24dbfb2b0e9d
Nick McArthur <mcarthurnick@gmail.com>
2016-04-18 22:33:32 -0600

    Initial Commit - ReactJSProgram

---
+++ app/index.js
@@ -0,0 +1,55 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var USER_DATA = {
+  name: 'Nick McArthur',
+  username: 'mcarthurnick',
+  image: 'https://avatars.githubusercontent.com/mcarthurnick'
+}
+
+
+var ProfilePic = React.createClass({
+  render: function() {
+    return
+      <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+  }
+});
+
+var ProfileLink = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <a href={'https://www.github.com/' + this.props.username}>
+          {this.props.username}
+        </a>
+    )
+  }
+});
+
+var ProfileName = React.createClass({
+  render: function() {
+    return (
+      <div>{this.props.name}</div>
+    )
+  }
+});
+
+
+var Avatar = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
+    )
+  }
+});
+
+
+
+ReactDOM.render(
+  <Avatar user={USER_DATA} />,
+  document.getElementById('app')
+);
